5a10f3a809df27aa097b31d817aa9bd300ac83ce
thuong.dp <thuong.dp@cloud9-solutions.com>
2022-12-29 10:28:08 +0700

    update

--- web/resources/views/pattern/pattern_detail_2.blade.php
+++ web/resources/views/pattern/pattern_detail_2.blade.php
@@ -112,11 +112,15 @@
                     </div>
                 </div>
             </div>
+            @if (!strpos(Request::root(), "solutions.com"))
             <br>
             Total Rows:<strong id="countRows"></strong>
             <br>
             <button onclick="setValueTest()">Set value test</button>
-            <br>
+            <br><br>
+            @else
+            <br><br><br>
+            @endif
             <div class="action-btn" style="justify-content: flex-start">
                 <button type="button" id="save" class="btn btn-success btn-ripple" style="margin-right: 1%;">
                     <div class="inside-btn">
