23393591ef9155711db662ab3eb4b724464a14f8
CLOUD9\duong.nv <duong.nv@cloud9-solutions.com>
2023-02-03 18:52:38 +0700

    Update layout

--- web/resources/views/pattern/pattern_top_page.blade.php
+++ web/resources/views/pattern/pattern_top_page.blade.php
@@ -3,7 +3,7 @@
 @push('css')
     <link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="Stylesheet"
         type="text/css"/>
-    {{-- <link href="{{ mix('/css/pattern.css') }}" rel="stylesheet"/> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ mix('/css/top_page.css') }}" rel="stylesheet"/>
 @endpush
 
