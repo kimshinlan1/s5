0526357ec3928f0a2035a310890e74f107bcd61d
CLOUD9\duong.nv <duong.nv@cloud9-solutions.com>
2023-02-08 18:23:33 +0700

    update

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -34,10 +34,9 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
     <!-- Styles -->
     <link rel="stylesheet" href="{!! url('assets/bootstrap/css/bootstrap-table.min.css') !!}">
+    <link rel="stylesheet" href="{!! url('assets/font-awesome-4.7.0/css/font-awesome.min.css') !!}">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @stack('css')
 </head>
