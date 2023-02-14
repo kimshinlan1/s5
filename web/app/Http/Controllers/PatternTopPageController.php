4f6e0153bd5eade2fa4f6768917cf92cb330672d
CLOUD9\duong.nv <duong.nv@cloud9-solutions.com>
2023-02-14 14:19:24 +0700

    update

--- web/app/Http/Controllers/PatternTopPageController.php
+++ web/app/Http/Controllers/PatternTopPageController.php
@@ -39,14 +39,10 @@ class PatternTopPageController extends Controller
      */
     public function generateDataHtml(Request $request)
     {
-        // todo:
         $companyId = $request->get('company_id');
-        $totalDeptPointArr = [0, 0, 0, 0, 0];
-        $avgDeptPointArr = null;
-        // $totalColumn = $request->get('new_total_column') ?: Constant::INSPECTION_DEFAULT_COLUMN_NUMBER;
 
         if (empty($companyId)) {
-            // todo:
+            $companyId = auth()->user()->company()->first()->id;
         }
 
         // Main data
@@ -58,10 +54,6 @@ class PatternTopPageController extends Controller
 
 
         foreach ($deptList as $key => $dept) {
-            $overallPoint = array();
-            $count = 0;
-            $totalDeptPointArr = [0, 0, 0, 0, 0];
-            $avgDeptPointArr = null;
             // Build struture
             $inspectionData[$key] = [
                 'dept_id' => $dept['id'],
@@ -84,49 +76,17 @@ class PatternTopPageController extends Controller
                     'inspections' => $inspectionDetails
                 ];
 
-                foreach ($inspectionDetails as $index => $inspectionDetail) {
-                    $count = $index;
-                    $tempArr = explode('|', $inspectionDetail['avg_point']);
-                    $tempStr = 'inspection' . strval($index);
-                    if (!array_key_exists($tempStr, $overallPoint)) {
-                        $overallPoint[$tempStr] = [0, 0, 0, 0, 0];
-                    }
-                    $overallPoint[$tempStr] = array_map(function () {
-                        return array_sum(func_get_args());
-                    }, $overallPoint[$tempStr], $tempArr);
-                }
-
                 if (count($inspectionDetails) > $maxColumn) {
                     $maxColumn = count($inspectionDetails);
                 }
             }
-            if ($count != 0) {
-                $avgDeptPointArr = array_map(function ($val) use ($count) {
-                    return round($val / $count, 2);
-                }, $totalDeptPointArr);
-            }
-
-            if ($avgDeptPointArr) {
-                $avgDeptPointArr = implode('|', $avgDeptPointArr);
-            }
-            // $inspectionData[$key]['dept_avgPoint'] = $overallPoint;
-            // $i = 0;
             // dd($inspectionData);
         }
-        // dd($deptList);
-        // Get ids to render columns
-        // $inspectionIds = array_keys($inspectionData);
-        // $i = 0;
-        // while (count($inspectionIds) < $totalColumn) {
-        //     $inspectionIds[] = "new_" . time() . $i;
-        //     $i++;
-        // }
 
         $companies = app()->get(CompanyService::class)->getAll()->toArray();
         Cache::put('companies', $companies, 10);
         $params = [
             'countInspection' => $maxColumn > Constant::INSPECTION_DEFAULT_COLUMN_NUMBER ? $maxColumn : Constant::INSPECTION_DEFAULT_COLUMN_NUMBER,
-            // 'inspectionIds' => $inspectionIds,
             'inspectionData' => $inspectionData,
             'companies' => Cache::get('companies')
         ];
