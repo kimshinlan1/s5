4ddbcbc3e5ded9bdee48b11c702810090c586e4f
du.pd <du.pd@cloud9-solutions.com>
2023-02-20 17:34:06 +0700

    Revert

--- web/resources/views/toppage/index.blade.php
+++ web/resources/views/toppage/index.blade.php
@@ -11,28 +11,28 @@
     <link href="{{ mix('/css/top_page.css') }}" rel="stylesheet"/>
 @endpush
 @section('content')
-<div class="row" style="margin-top: 100px; display: flex; flex-wrap: wrap;">
+<div class="row" style="margin-top: 100px;">
     @if(auth()->user()->isAdmin())
-    <div class="col-sm" style="min-width: 225px">
+    <div class="col-sm">
         <a onclick="setSession('menu1')" id="managementLinkId" class="main-menu list-group-item list-group-item-action style-list @if(request()->path()==='users')active @endif" href="/users">
             <i class="fa fa-cog mx-1" aria-hidden="true"></i>
             {{ __('TopPage_Common_Management') }}
         </a>
     </div>
     @endif
-    <div class="col-sm" style="min-width: 225px">
+    <div class="col-sm">
         <a onclick="setSession('menu2')" id="studyPlanLinkId" class="main-menu list-group-item list-group-item-action style-list @if(request()->path()==='study_plan')active @endif" href="/study_plan">
             <i class="fa fa-graduation-cap mx-1" aria-hidden="true"></i>
             {{ __('TopPage_Study_Plan_Management') }}
         </a>
     </div>
-    <div class="col-sm" style="min-width: 225px">
+    <div class="col-sm">
         <a onclick="setSession('menu3')" id="skillMapLinkId" class="main-menu list-group-item list-group-item-action @if(str_contains(request()->path(), 'skillmaps'))active @endif" href="{{ route('skillmap_list') }}">
             <i class="fa fa-line-chart mx-1" aria-hidden="true"></i>
             {{ __('TopPage_Skill_Map') }}
         </a>
     </div>
-    <div class="col-sm" style="min-width: 225px">
+    <div class="col-sm">
         <a onclick="setSession('menu4')" id="topPageLinkId" class="main-menu list-group-item list-group-item-action style-list @if(request()->path()==='pattern_top_page')active @endif" href="/pattern_top_page">
             <i class="fa fa-bar-chart mx-1" aria-hidden="true"></i>
             {{ __('TopPage_5S_System') }}
