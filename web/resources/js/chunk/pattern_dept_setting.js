e7e682cf42e809de704891213d9319a046d26532
CLOUD9\duong.nv <duong.nv@cloud9-solutions.com>
2023-04-03 18:21:09 +0700

    update

--- web/resources/js/chunk/pattern_dept_setting.js
+++ web/resources/js/chunk/pattern_dept_setting.js
@@ -536,34 +536,6 @@ window.checkBindDeptPattern = function(deptId, patternId) {
     return res;
 }
 
-window.getNow = function() {
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    let day = now.getDate();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let second = now.getSeconds();
-
-    if (month.toString().length === 1) {
-    month = '0' + month;
-    }
-    if (day.toString().length === 1) {
-    day = '0' + day;
-    }
-    if (hour.toString().length === 1) {
-    hour = '0' + hour;
-    }
-    if (minute.toString().length === 1) {
-    minute = '0' + minute;
-    }
-    if (second.toString().length === 1) {
-    second = '0' + second;
-    }
-
-    return year.toString().substr(-2) + month + day + hour + minute + second;
-}
-
 /////////////////////////////////////////////////////////////////////////////
 
 /**
