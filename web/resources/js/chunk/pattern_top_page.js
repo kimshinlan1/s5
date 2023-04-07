fb1b7d1ef8436821f2a518d546cd210cd001cdf8
CLOUD9\duong.nv <duong.nv@cloud9-solutions.com>
2023-04-07 18:43:48 +0700

    update

--- web/resources/js/chunk/pattern_top_page.js
+++ web/resources/js/chunk/pattern_top_page.js
@@ -316,4 +316,24 @@ $(function () {
     } else {
       $('#companyOptionId').change();
     }
+
+
+  let deptBtn = document.querySelector(".dept-btn");
+  // let teamBtn = document.querySelector("#btnInput");
+
+  // Add overflow class if needed
+  if (deptBtn.scrollWidth > deptBtn.offsetWidth) deptBtn.classList.add("has-overflow");
+
+  while (deptBtn.scrollWidth > deptBtn.offsetWidth) {
+    // Remove characters from paragraph until the text and the overflow indicator fit
+    deptBtn.innerHTML = deptBtn.innerHTML.slice(0, -1);
+  }
+
+  // // Add overflow class if needed
+  // if (teamBtn.scrollWidth > teamBtn.offsetWidth) teamBtn.classList.add("has-overflow");
+
+  // while (teamBtn.scrollWidth > teamBtn.offsetWidth) {
+  //   // Remove characters from paragraph until the text and the overflow indicator fit
+  //   teamBtn.innerHTML = teamBtn.innerHTML.slice(0, -1);
+  // }
 });
