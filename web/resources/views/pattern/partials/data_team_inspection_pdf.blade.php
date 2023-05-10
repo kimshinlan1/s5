4879231d952d0f01afa015c4c294174ce0bfde3c
CLOUD9\duong.nv <duong.nv@cloud9-solutions.com>
2023-05-10 10:27:59 +0700

    update

--- web/resources/views/pattern/partials/data_team_inspection_pdf.blade.php
+++ web/resources/views/pattern/partials/data_team_inspection_pdf.blade.php
@@ -5,17 +5,19 @@ $areas = [];
 $locations = [];
 
 ?>
+<!DOCTYPE html>
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    {{-- <link rel="stylesheet" href= "{!! url('assets/bootstrap-3.4.1/css/bootstrap.min.css') !!}"/>
+    <script src="{!! url('assets/bootstrap-3.4.1/js/bootstrap.min.js') !!}" type="text/javascript"></script> --}}
     <style>
-        {
             @font-face {
                 font-family: 'yugothib';
                 font-style: normal;
                 font-weight: normal;
                 src: url("{{ public_path('assets/font/yu-gothic/yugothib.ttf') }}") format('truetype');
             }
-
             * {
                 font-family: 'yugothib';
                 font-size: 10px;
@@ -23,7 +25,6 @@ $locations = [];
             body {
                 font-family: 'yugothib';
             }
-
             table {
                 border-collapse: collapse;
                 margin: auto;
@@ -31,19 +32,44 @@ $locations = [];
                 table-layout: fixed;
             }
 
+            tr {
+                page-break-inside: avoid !important;
+            }
             td,
             th {
                 border: 1px solid black;
-                width: 20%;
-                height: 30px;
-                page-break-inside: avoid;
+                width: 12.5%;
+                height: 30px !important;
+                page-break-inside: avoid !important;
                 word-wrap: break-word;
+                font-size: 16px;
+            }
+            .point-class {
+                text-align: center;
             }
             h1 {
-                page-break-before: always;
+                font-size: 30px;
+                font-weight: bolder;
             }
-
-        }
+            .level-sub-table {
+                padding-right: 0px;
+                margin: 0px 15px 0px;
+                width: 90%;
+                height: 100%;
+                border-color: red;
+            }
+            .tr-sub-table {
+                width: 100%;
+                height: 100%;
+            }
+            .td-sub-table {
+                width:85%;
+                height: 100%;
+                padding-left: 10px;
+                margin: 0px;
+                border-color: transparent;
+                border-left-color: black
+            }
     </style>
 </head>
 <body>
@@ -53,7 +79,7 @@ $locations = [];
     <span style="font-size: 20px; margin-right: 15px">{{ $teamName }}</span>
 </div>
 <div style="text-align: center">
-    <h1 style="font-size: 20px">5Sチェックリスト</h1>
+    <h1>5Sチェックリスト</h1>
 </div>
 <table class="data_table" cellspacing="0" cellpadding="0">
 
@@ -61,7 +87,7 @@ $locations = [];
         <tr style="height: 40.5px;" id="header-2-table-content">
             <th class="value-area" scope="col">点検エリア</th>
             <th class="value-location" scope="col">点検箇所</th>
-            <th class="value-5s" scope="col">改善ポイント</th>
+            <th class="value-5s point-class" scope="col">改善ポイント</th>
             <th class="value-level" scope="col">最悪（レベル１）</th>
             <th class="value-level" scope="col">悪い（レベル2） </th>
             <th class="value-level" scope="col">悪い（レベル3） </th>
@@ -87,7 +113,7 @@ $locations = [];
             {{-- Area --}}
             @if (!in_array($row['area_id'], $areas))
                 <td rowspan="{{ $row['area_rowspan'] }}" class="area"
-                    style="background-color: #d8e4bc; text-align:center; page-break-inside: avoid;">
+                    style="background-color: #d8e4bc; text-align:center;">
                     {{ $row['area_name'] }}
                 </td>
                 <?php $areas[] = $row['area_id']; ?>
@@ -96,23 +122,22 @@ $locations = [];
             {{-- Locations --}}
             @if (!in_array($locaitonIdToCheck, $locations))
                 <td rowspan="{{ $row['location_rowspan'] }}" class="location"
-                    style="background-color: #d8e4bc; text-align:center; page-break-inside: avoid; ">
+                    style="background-color: #d8e4bc; text-align:center; vertical-align: center;">
                     {{ $row['location_name'] }}
                 </td>
                 <?php $locations[] = $locaitonIdToCheck; ?>
             @endif
 
             {{-- Point --}}
-            <td>
+            <td class="point-class">
                 {{ Constant::NAME_5S[$row['5s']] }}
             </td>
 
             {{-- Levels --}}
             <td>
-                <table style=" padding-right: 0px; margin-left: 15px; margin-right: 0px; width:100%">
-                    <tr style="width:100%">
-                        <td
-                            style=" width:85%; border:none;border-left: 1px solid; padding-left: 10px; margin-right: 0px;">
+                <table class="level-sub-table">
+                    <tr class="tr-sub-table">
+                        <td class="td-sub-table">
                                 @if(isset($row['level_1']))
                                     {{ $row['level_1'] }}
                                 @else
@@ -123,10 +148,9 @@ $locations = [];
                 </table>
             </td>
             <td>
-                <table style=" padding-right: 0px; margin-left: 15px; margin-right: 0px; width:100%">
-                    <tr style="width:100%">
-                        <td
-                            style=" width:85%; border:none;border-left: 1px solid; padding-left: 10px; margin-right: 0px;">
+                <table class="level-sub-table">
+                    <tr class="tr-sub-table">
+                        <td class="td-sub-table">
                                 @if(isset($row['level_2']))
                                     {{ $row['level_2'] }}
                                 @else
@@ -137,10 +161,9 @@ $locations = [];
                 </table>
             </td>
             <td>
-                <table style=" padding-right: 0px; margin-left: 15px; margin-right: 0px; width:100%">
-                    <tr style="width:100%">
-                        <td
-                            style=" width:85%; border:none;border-left: 1px solid; padding-left: 10px; margin-right: 0px;">
+                <table class="level-sub-table">
+                    <tr class="tr-sub-table">
+                        <td class="td-sub-table">
                                 @if(isset($row['level_3']))
                                     {{ $row['level_3'] }}
                                 @else
@@ -151,10 +174,9 @@ $locations = [];
                 </table>
             </td>
             <td>
-                <table style=" padding-right: 0px; margin-left: 15px; margin-right: 0px; width:100%">
-                    <tr style="width:100%">
-                        <td
-                            style=" width:85%; border:none;border-left: 1px solid; padding-left: 10px; margin-right: 0px;">
+                <table class="level-sub-table">
+                    <tr class="tr-sub-table">
+                        <td class="td-sub-table">
                                 @if(isset($row['level_4']))
                                     {{ $row['level_4'] }}
                                 @else
@@ -165,10 +187,9 @@ $locations = [];
                 </table>
             </td>
             <td>
-                <table style=" padding-right: 0px; margin-left: 15px; margin-right: 0px; width:100%">
-                    <tr style="width:100%">
-                        <td
-                            style=" width:85%; border:none;border-left: 1px solid; padding-left: 10px; margin-right: 0px;">
+                <table class="level-sub-table">
+                    <tr class="tr-sub-table">
+                        <td class="td-sub-table">
                                 @if(isset($row['level_5']))
                                     {{ $row['level_5'] }}
                                 @else
@@ -187,3 +208,4 @@ $locations = [];
 </table>
 
 </body>
+</html>
